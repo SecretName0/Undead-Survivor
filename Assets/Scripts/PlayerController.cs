abf9fcd9e87c645be7ac7292a3efd74116e0c56f
SecretName0 <dlwnsdud502@naver.com>
2024-05-19 18:11:31 +0900

    2024 05 19
    Update
    - Player Characters
    - Infinite Map

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [NonSerialized] public Vector2 InputVector;
+
+    // Character Value
+    [SerializeField] float MoveSpeed;
+
+    // Components
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        GameManager.Instance.Player = this;
+    }
+
+
+    private void FixedUpdate()
+    {
+        /* 물리 이동의 기본적인 논리 순서
+        // 힘을 가함
+        rb.AddForce(InputVector);
+
+        // 속도 제어
+        rb.velocity = InputVector;
+
+        // 위치 이동
+        rb.MovePosition(rb.position + InputVector);
+        */
+
+        Vector2 NextVector = InputVector * MoveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + NextVector);
+    }
+
+    void Update()
+    {
+        UpdateAnimation();
+    }
+
+    private void LateUpdate()
+    {
+        if (InputVector.x < 0)
+        {
+            sr.flipX = true;
+        }
+
+        else if(InputVector.x > 0)
+        {
+            sr.flipX = false;
+        }
+    }
+
+    void InputValue()
+    {
+
+    }
+
+    void OnMove(InputValue Value)
+    {
+        InputVector = Value.Get<Vector2>();
+    }
+
+    void UpdateAnimation()
+    {
+        if(InputVector != Vector2.zero)
+        {
+            anim.SetBool("IsMoving", true);
+        }
+
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
+    }
+}
---
+++ Assets/Scripts/Reposition.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    Vector3 PlayerPos; // 플레이어 위치
+    Vector3 MyPos; // 이 스크립트를 가진 객체의 위치
+    Vector3 PlayerDir; // 플레이어가 나아가는 방향
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area")) // 벗어난 게 플레이어의 에리어가 아닌 경우 그냥 반환
+            return;
+
+        PlayerPos = GameManager.Instance.Player.transform.position;
+        MyPos = transform.position;
+
+        float DifX = Mathf.Abs(PlayerPos.x - MyPos.x); // 플레이어와의 x축 거리차
+        float DifY = Mathf.Abs(PlayerPos.y - MyPos.y); // 플레이어와의 y축 거리차
+
+        PlayerDir = GameManager.Instance.Player.InputVector;
+        float DirX = PlayerDir.x < 0 ? -1 : 1;
+        float DirY = PlayerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if(DifX > DifY) // 플레이어가 X축으로 멀어지는 경우
+                {
+                    transform.Translate(Vector3.right * DirX * 40); // 타일맵이 4개가 있고 축당 길이가 개당 20으로 2x2사이즈라 40만큼
+                }
+
+                if (DifX < DifY) // 플레이어가 Y축으로 멀어지는 경우
+                {
+                    transform.Translate(Vector3.up * DirY * 40); // 타일맵이 4개가 있고 축당 길이가 개당 20으로 2x2사이즈라 40만큼
+                }
+
+                break;
+
+            case "Enemy":
+
+                break;
+        }
+    }
+}
